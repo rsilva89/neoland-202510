bfd58b201f72938b5f89283053e446cb87a9c46e
Ricardo Silva Laredo <richiesilva@Mac-mini.local>
2025-11-11 13:13:00 +0100

    add series to js

--- staff/ricardo-silva/playground/js/intro.js
+++ staff/ricardo-silva/playground/js/intro.js
@@ -265,19 +265,19 @@ Date                        ../../....
 
 var alumnos = []
 
-alumnos[0] = {nombre: 'Juan', edad: 34}
-alumnos[1] = {nombre: 'Laura', edad: 30}
-alumnos[2] = {nombre: 'Albert', edad: 36}
-alumnos[3] = {nombre: 'Jodlin', edad: 45}
-alumnos[4] = {nombre: 'Paula', edad: 32}
-alumnos[5] = {nombre: 'Manuel', edad: 15}
-alumnos[6] = {nombre: 'Jorge', edad: 32}
-alumnos[7] = {nombre: 'Sergio', edad: 30}
-alumnos[8] = {nombre: 'Rodolfo', edad: 36}
-alumnos[9] = {nombre: 'Stephany', edad: 27}
-alumnos[10] = {nombre: 'Agustin', edad: 23}
-alumnos[11] = {nombre: 'Patricia', edad: 23}
-alumnos[12] = {nombre: 'Ibra', edad: 18}
+alumnos[0] = {nombre: 'Juan', edad: 34, series: ['The Big Bang Theory','Peaky Blinders','Game of Thrones']}
+alumnos[1] = {nombre: 'Laura', edad: 30, series: ['Mindhunter','Narcos','Monstruos']}
+alumnos[2] = {nombre: 'Albert', edad: 36, series: ['Avatar','Outlander','One Piece']}
+alumnos[3] = {nombre: 'Jodlin', edad: 45, series: ['Two and half men','Aqui no hay quien viva','La que se avecina']}
+alumnos[4] = {nombre: 'Paula', edad: 32, series: ['Breaking Bad','The Boys','Attack of Titans']}
+alumnos[5] = {nombre: 'Manuel', edad: 15, series: []}
+alumnos[6] = {nombre: 'Jorge', edad: 32, series: ['The Office','One Piece','Ted Lasso']}
+alumnos[7] = {nombre: 'Sergio', edad: 30, series: ['Pose','Attack of Titans','Severance']}
+alumnos[8] = {nombre: 'Rodolfo', edad: 36, series: ['Fringe','How I met your mother']}
+alumnos[9] = {nombre: 'Stephany', edad: 27, series: ['Greys Anatomy','House of Dragon','Friends']}
+alumnos[10] = {nombre: 'Agustin', edad: 23, series: ['Sobrenatural','Perdidos','Majime no ippo']}
+alumnos[11] = {nombre: 'Patricia', edad: 23, series: ['Girls','Sex and the City','The Handmind fale']}
+alumnos[12] = {nombre: 'Ibra', edad: 18, series: ['Luppin']}
 
 /*
 for (var i = 0; i < alumnos.length; i++ ) {
@@ -290,6 +290,9 @@ var suma = 0
 //ahora vamos a ir de arriba hacia abajo
 for (var i = alumnos.length-1; i >= 0; i-- ) {
     console.log( 'Nombre: ' + alumnos[i].nombre.toLowerCase() + ' Edad ' + '(' + alumnos[i].edad + ')' )
+    for(var j = 0; j < alumnos[i].series.length; j++ ) {
+        console.log( 'Series: ' + alumnos[i].series[j])
+    }
     suma += alumnos[i].edad
 }
 
