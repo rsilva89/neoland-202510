a3dbda48295a190efe20bcc809dd875853bda562
Ricardo Silva Laredo <richiesilva@Mac-mini.local>
2025-11-19 19:07:32 +0100

    add var-let-const snippet

---
+++ staff/ricardo-silva/playground/js/var-let-const.js
@@ -0,0 +1,33 @@
+/* var */
+
+a = 1
+
+var a // hoisting
+
+console.log(a)
+
+var a // no problem
+
+/* let */
+
+//b = 2 // not allowed
+
+let b
+
+b = 2
+
+console.log(b)
+
+//let b // not allowed
+
+/* const */
+
+const c = 3
+
+//c = 4 // not allowed
+
+console.log(c)
+
+// not allowed
+//const d
+//d = 4
